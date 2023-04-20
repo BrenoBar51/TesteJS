5b82523b6ea86c048f71a39764dbfb7a7adef65d
BrenoBar51 <brenobarcelos9@gmail.com>
2023-04-19 22:04:10 -0300

    Calculo de media corrigido

--- logica-treino.js
+++ logica-treino.js
@@ -1,25 +1,14 @@
-/*const idade = prompt('coloque sua idade');
-console.log(idade)
-if(idade > 17) {
-  console.log('Pode entrar e encher a cara')
-}
-else if(idade < 18){
-  console.log('Fica só olhando')
-}*/
-const nota1 = prompt('nota 1')
-const nota2 = prompt('nota 2')
-const nota3 = prompt('nota 3')
-const nota4 = prompt('nota 4')
-const nota5 = prompt('nota 5')
-const nota6 = prompt('nota 6')
-const media = (nota1+nota2+nota3+nota4+nota5+nota6) / 6
-console.log(nota1,nota2,nota3,nota4,nota5,nota6,media)
+const nota1 = parseFloat(prompt('Digite a primeira nota:'));
+const nota2 = parseFloat(prompt('Digite a segunda nota:'));
+const media = (nota1 + nota2)/2;
+console.log('Notas:', nota1, nota2, 'Média:', media);
+
 if(media === 10){
-  console.log('Passou e ainda frescou')
+  console.log('Passou e ainda frescou');
 }
-else if(media <= 7 && = 8 ){
-  console.log('Passou morrendo')
+else if(media < 7){
+  console.log('Vai repetir de ano');
 }
 else if(media >= 7){
-  console.log('Vai repetir de ano..., de novo')
+  console.log('Passou');
 }
