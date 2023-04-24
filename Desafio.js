a584413219efdc5072a5cdabd54908c55ecbaf7e
BrenoBar51 <brenobarcelos9@gmail.com>
2023-04-24 09:53:40 -0300

    Novas atividades js

---
+++ Desafio.js
@@ -0,0 +1,49 @@
+/* calcular numero par ou impar(1)
+const valor = prompt('digite aqui o numero:')
+valor %2 == 0 ? console.log ("par") : console.log("impar");
+*/
+/* idade para votar(2)
+const idade = prompt('digite aqui sua idade:')
+if(idade >= 16 && idade <= 70){
+  console.log('pode votar')
+}
+else if(idade >= 70 ){
+  console.log('Não precisa votar')
+}
+else if(idade <= 15){
+  console.log('Não pode votar')
+}*/
+/* positivo , negativo ou zero(3)
+const numero = prompt('Coloque aqui um numero:')
+if(numero >= 1){
+  console.log('positivo')
+}
+else if(numero <= -1){
+  console.log('negativo')
+}
+else if(numero == 0){
+  console.log('zero')
+}*/
+/* idade para dirigir(4)
+const idade = prompt("Coloque aqui sua idade")
+if (idade >= 18){
+  console.log('Pode dirigir')
+}
+else{
+  console.log('Não pode dirigir')
+}*/
+/* media de nota escolar(5)
+const nota1 = parseFloat(prompt('Digite a primeira nota:'));
+const nota2 = parseFloat(prompt('Digite a segunda nota:'));
+const media = (nota1 + nota2)/2;
+console.log('Notas:', nota1, nota2, 'Média:', media);
+
+if(media === 10){
+  console.log('Passou e ainda frescou');
+}
+else if(media < 7){
+  console.log('Vai repetir de ano');
+}
+else if(media >= 7){
+  console.log('Passou');
+}*/
--- operadores-mtm.js
+++ operadores-mtm.js
@@ -1,15 +1,11 @@
-/*const inputValue = document.getElementById('titulo')
+const inputValue = document.getElementById('titulo')
 const valor1 = prompt('valor 1')
-const valor2 = prompt('valor 2')
 
-const multi = valor1 * valor2
-
-inputValue.innerHTML = multi
-
-const par = valor1 / 2
+const par = valor1 % 2
+const impar = 
 console.log(par)
 if(par === 0) {
   console.log(true)
 } else{
   console.log(false)
-}*/
+}
