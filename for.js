8997e8036dbe01c7822a92a10f0bf3abd84c9dcc
BrenoBar51 <brenobarcelos9@gmail.com>
2023-05-03 08:59:24 -0300

    For e for off

---
+++ for.js
@@ -0,0 +1,36 @@
+/**
+ * Repetições com for
+ * for - iterações sobre o indice
+ * for...in - iterações sobre os valores de um objeto
+ * for...of - iterações sobre os valores de diversos tipos de dados 
+ */
+/*const div = document.getElementById('exemplo')
+
+const arrayFor = [1, 2, 3, 4, 5, 6]
+//                0, 1, 2, 3, 4, 5
+for (let i = 0; i < arrayFor.length; i++){
+    div.innerHTML += arrayFor[i]
+    console.log(arrayFor[i])
+}
+
+const objeto = {
+    "nome": "Breno",
+    "idade": 18,
+    "profissão": "Várias"
+}
+
+for (let value in objeto){
+    console.log(`${value}: ${objeto[value]}`)
+}
+
+const nomes = ['Breno', 'Barcelos', 'De Oliveira']
+
+for (let nome of nomes){
+    console.log(nome)
+}
+
+const usuarios = {objeto, objeto, objeto, objeto}
+
+for (let usuario of usuarios){
+    console.log(usuario.nome)
+}*/
