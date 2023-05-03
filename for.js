31a044f1d727df72d1ce120366cb105fbdb397d0
BrenoBar51 <brenobarcelos9@gmail.com>
2023-05-03 09:25:15 -0300

    tabuada

--- for.js
+++ for.js
@@ -34,3 +34,28 @@ const usuarios = {objeto, objeto, objeto, objeto}
 for (let usuario of usuarios){
     console.log(usuario.nome)
 }*/
+/*const div = document.getElementById('exemplo')
+const arrayFor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numero = parseInt()
+
+for (let i = 0; i < arrayFor.length; i++){
+    div.innerHTML += arrayFor[i]
+    console.log(arrayFor[i])
+}
+
+if (valor === 1) {
+    console.log(arrayFor * 1)
+}
+else if (valor === 2) {
+    console.log(arrayFor * 2)
+}*/
+const numero = parseInt(prompt('Digite aqui um numero de 1 a 10'))
+
+for (let i = 1; i <= 10; i++){
+    const resultado = numero * i;
+    let tabuada = '';
+    for(const multiplicador of [numero, i, resultado]) {
+        tabuada += `${multiplicador}\t`;
+    }
+    console.log(tabuada);
+}
